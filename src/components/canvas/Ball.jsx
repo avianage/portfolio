699b41ad8ac24510359ba86bab82faf4f87b7768
avianage <aakashj853@gmail.com>
2024-12-06 17:49:06 +0530

    edited canvas

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,5 @@
 import React, { Suspense } from "react";
+import PropTypes from "prop-types";
 import { Canvas } from "@react-three/fiber";
 import {
   Decal,
@@ -10,17 +11,25 @@ import {
 
 import CanvasLoader from "../Loader";
 
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+const Ball = ({ imgUrl, floatProps }) => {
+  const [decal] = useTexture([imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
+    <Float
+      speed={floatProps.speed}
+      rotationIntensity={floatProps.rotationIntensity}
+      floatIntensity={floatProps.floatIntensity}
+    >
+      {/* Lighting setup */}
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[0, 0, 1]} intensity={0.8} />
+      <pointLight position={[10, 10, 10]} intensity={1} />
+
+      {/* Main Ball Mesh */}
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color='#fff8eb'
+          color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
@@ -38,20 +47,36 @@ const Ball = (props) => {
 };
 
 const BallCanvas = ({ icon }) => {
+  const floatProps = {
+    speed: 2, // Speed of floating effect
+    rotationIntensity: 1.5, // Intensity of rotation
+    floatIntensity: 3, // Intensity of floating
+  };
+
   return (
     <Canvas
-      frameloop='demand'
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
+      frameloop="demand"
+      dpr={[1, 2]} // Adjust resolution dynamically
+      gl={{ preserveDrawingBuffer: true, antialias: true }} // Enable anti-aliasing
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
+        <Ball imgUrl={icon} floatProps={floatProps} />
       </Suspense>
 
+      {/* Preload textures for better performance */}
       <Preload all />
     </Canvas>
   );
+};
+
+/* Prop Validation */
+BallCanvas.propTypes = {
+  icon: PropTypes.string.isRequired, // Ensure `icon` is a valid string (URL or path)
+};
+
+BallCanvas.defaultProps = {
+  icon: "/default-image.png", // Fallback image
 };
 
 export default BallCanvas;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,27 +1,35 @@
-import { Suspense, useEffect, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import CanvasLoader from '../Loader';
+import CanvasLoader from "../Loader";
 
-const Computers = ( { isMobile } ) => {
-  const { scene } = useGLTF('/desktop_pc/scene.gltf');
+const Computers = ({ isMobile }) => {
+  const { scene } = useGLTF("/desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={2} groundColor="black" />
-      <pointLight intensity={2}/>
+      {/* Lighting Setup */}
+      <hemisphereLight intensity={0.35} groundColor="black" />
+      <directionalLight
+        position={[10, 10, 5]}
+        intensity={1}
+        castShadow
+        shadow-mapSize={[1024, 1024]}
+      />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={2}
+        intensity={1.5}
         castShadow
-        shadow-mapSize={1024}
+        shadow-mapSize={[1024, 1024]}
       />
+      
+      {/* 3D Model */}
       <primitive
         object={scene}
-        scale={isMobile ? 0.7 : 0.75 } 
+        scale={isMobile ? 0.7 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -33,38 +41,40 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
 
+    // Set initial state
     setIsMobile(mediaQuery.matches);
 
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    }
+    // Listener for screen resizing
+    const handleMediaQueryChange = (event) => setIsMobile(event.matches);
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    mediaQuery.addEventListener('change', handleMediaQueryChange);
-
-    return () => {
-      mediaQuery.removeEventListener('change', handleMediaQueryChange);
-    }
-
-  }, [])
-  
+    // Cleanup event listener on unmount
+    return () => mediaQuery.removeEventListener("change", handleMediaQueryChange);
+  }, []);
 
   return (
     <Canvas
-      frameLoop="demand"
+      frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
+      gl={{ preserveDrawingBuffer: true, antialias: true }}
+      dpr={[1, 2]} // Adjust for performance and resolution
     >
       <Suspense fallback={<CanvasLoader />}>
+        {/* Camera and Controls */}
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile}/>
+
+        {/* 3D Model */}
+        <Computers isMobile={isMobile} />
       </Suspense>
+
+      {/* Preload Assets */}
       <Preload all />
     </Canvas>
   );
